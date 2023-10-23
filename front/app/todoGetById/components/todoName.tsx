e61e684ea02d28950baabcdb2440bee9ed98c5f8
KensukeMiyata <k1206miyata@gmail.com>
2023-10-23 15:03:20 +0900

    todo更新ページの更新

--- front/app/todoGetById/components/todoName.tsx
+++ front/app/todoGetById/components/todoName.tsx
@@ -1,21 +1,31 @@
 import React, { } from 'react';
 
+// todoの名前をgetするためだけに型指定。適当に型指定している
+
 type Todo = {
     id: string,
     content: string,
     complete: boolean,
     deadline: string,
-    createdAt: string,
-    updatedAt: string,
+    created_at: string,
+    updated_at: string,
   };
 
-export default function TodoName() {
-    // const response = await fetch('http://localhost:3000/todoGetById/api/getTags');
-    // if (!response.ok) throw new Error('Failed to fetch data');
-    // const todo: Todo[] = await response.json();
+export default async function TodoName() {
+    const response = await fetch('http://localhost:3000/todoGetById/api/getTodo');
+    if (!response.ok) throw new Error('Failed to fetch data');
+    const todo: Todo[] = await response.json();
     return (
         <div className="px-4 py-5 sm:px-6 bg-indigo-600">
-            <h1 className="text-lg font-semibold text-white">todo</h1>
+            {/* <h1 className="text-lg font-semibold text-white"> */}
+                {todo.map((todo) => (<h1 key={todo.id}>{todo.content}
+                </h1>))}
+                {/* </h1> */}
+            {/* <ul>
+                {todo.map((todo) => (
+                    <li key={todo.id}>{todo.content}</li>
+                ))}
+            </ul> */}
         </div>
     );
 }
