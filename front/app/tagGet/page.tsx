34e4d43af99c0db6779dbcedc04b3d0d76b27cee
KensukeMiyata <k1206miyata@gmail.com>
2023-10-24 12:32:45 +0900

    tag一覧画面仮完了

--- front/app/tagGet/page.tsx
+++ front/app/tagGet/page.tsx
@@ -6,13 +6,13 @@ import Put from "../todoGetById/components/put";
 import PostTag from "./postTag";
 
 
-export default function TodoGetById() {
+export default function TagGetAll() {
 
   return (
     <main className="min-h-screen py-10 px-4 sm:px-6 lg:px-8 max-w-5xl mx-auto">
 
         <div className="">
-            <div className="w-full m-5 bg-white">
+            <div className="w-full m-5">
                 <div className="max-w-md mx-auto rounded-lg shadow-md overflow-hidden">
                     <GetTags/>
                 </div>
