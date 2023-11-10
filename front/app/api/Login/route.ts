1f24b662bd2343a834ca0e5484152d593e73a94c
mz-akihiro <2000akihiromizuno@gmail.com>
2023-11-10 20:14:39 +0900

    トークンを発行してクッキーに付与する機能を追加

--- front/app/api/Login/route.ts
+++ front/app/api/Login/route.ts
@@ -1,33 +1,50 @@
 import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
 
+export async function POST(req: NextRequest) {
 
-export async function POST(req: Request) {
+  console.log("<<< test >>>")
+  const body = await req.text();
+  console.log(body);
+  const res_js = await JSON.parse(body)
+  console.log(res_js.email)
 
-    console.log("<<< test >>>")
-    const body = await req.text();
-    console.log(body);
-    const res_js = await JSON.parse(body)
-    console.log(res_js.email)
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
 
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
+  let responseStatus
+  let responseUserId
 
-    let responseStatus
-    await fetch('http://localhost:8000/v1/user/login', {
-        method: 'POST',
-        body: JSON.stringify(res_js),
-        headers: headers,
-    })
-    .then(res => {
-        responseStatus = res.status;
-        return res.json();
-    })
-    .then(json => {
-      console.log("server side success : ", json)
-    })
-    .catch(error => {
-      console.error('server side error : ', error)
-    })
-    console.log(responseStatus)
-    return NextResponse.json({ status: responseStatus });
+  await fetch('http://localhost:8000/v1/user/login', {
+      method: 'POST',
+      body: JSON.stringify(res_js),
+      headers: headers,
+  })
+  .then(res => {
+      responseStatus = res.status;
+      return res.json();
+  })
+  .then(json => {
+    console.log("server side success : ", json)
+    responseUserId = json
+  })
+  .catch(error => {
+    console.error('server side error : ', error)
+  })
+  console.log(responseStatus)
+
+  const response = NextResponse.json({ status: responseStatus })
+
+  if (300 > responseStatus) {
+    const secretKey = 'your-secret-key';  // .envに書き込む
+
+    const payload = {
+      user_id: responseUserId
+    };
+
+    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+    response.cookies.set('Set-Cookie', `token=${token}; HttpOnly; Path=/; Max-Age=${60 * 60}`);
+  }
+  return response
 }
