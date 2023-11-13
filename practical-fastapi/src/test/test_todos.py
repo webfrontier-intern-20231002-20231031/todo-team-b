b8bbdf8089121c0d70026ef969bf03e82b5db486
mrAkito <fa1con_well@outlook.jp>
2023-11-13 18:54:38 +0900

    pytestの分割

--- practical-fastapi/src/test/test_todos.py
+++ practical-fastapi/src/test/test_todos.py
@@ -4,9 +4,12 @@ from app.database import get_db
 from app.main import app
 from starlette.responses import Content
 
+import pytest
+
 client = TestClient(app)
 
 
+@pytest.mark.run_these_todo
 def temp_db(f):
     def func(SessionLocal, *args, **kwargs):
         def override_get_db():
@@ -22,6 +25,7 @@ def temp_db(f):
 
     return func
 
+@pytest.mark.run_these_todo
 # 各todoが取得できることを確認
 @temp_db
 def test_get_todo():
@@ -31,6 +35,8 @@ def test_get_todo():
     assert res_js[0]["content"] == "買い物する"
     assert res_js[1]["content"] == "帰宅する"
 
+
+@pytest.mark.run_these_todo
 # 1つ目のtodoに紐づいているtagをそれぞれ確認
 @temp_db
 def test_get_todo_tag():
@@ -40,6 +46,7 @@ def test_get_todo_tag():
     assert res_js[0]["tags"][0]["id"] == 1
     assert res_js[0]["tags"][1]["name"] == "カフェオレを作る"
 
+@pytest.mark.run_these_todo
 # id指定でtodoを取得できるか確認
 @temp_db
 def test_get_todo_by_id():
@@ -48,6 +55,7 @@ def test_get_todo_by_id():
     res_js = response.json()
     assert res_js["content"] == "買い物する"
 
+@pytest.mark.run_these_todo
 # id指定で取得したtodoに紐づいているtagを確認
 @temp_db
 def test_get_todo_by_id_tag():
@@ -56,6 +64,7 @@ def test_get_todo_by_id_tag():
     res_js = response.json()
     assert res_js["tags"][0]["id"] == 1
 
+@pytest.mark.run_these_todo
 # todoの新規作成を確認
 @temp_db
 def test_create_todo():
@@ -67,6 +76,7 @@ def test_create_todo():
     res_js = response.json()
     assert res_js["content"] == "test todo"
 
+@pytest.mark.run_these_todo
 # completedの変更をチェックします
 @temp_db
 def test_update_todo_completed():
@@ -80,6 +90,7 @@ def test_update_todo_completed():
     res_js = response_check.json()
     assert res_js["completed"] == True
 
+@pytest.mark.run_these_todo
 # todoに紐づいているtagを変更する
 @temp_db
 def test_update_todo_change_tag():
@@ -95,6 +106,7 @@ def test_update_todo_change_tag():
     res_js = response_check.json()
     assert res_js["tags"][0]["name"] == "コーヒーを買う"
 
+@pytest.mark.run_these_todo
 # todoに新規で作成したtagを紐付ける
 @temp_db
 def test_update_todo_add_newtag():
@@ -110,6 +122,7 @@ def test_update_todo_add_newtag():
     res_js = response_check.json()
     assert res_js["tags"][0]["name"] == "新規タグ"
 
+@pytest.mark.run_these_todo
 # todoに紐付いているタグを削除する
 @temp_db
 def test_update_todo_delete_tag():
@@ -123,6 +136,7 @@ def test_update_todo_delete_tag():
     res_js = response_check.json()
     assert res_js["tags"] == []
 
+@pytest.mark.run_these_todo
 # todoの削除を確認
 @temp_db
 def test_delete_todo():
@@ -132,12 +146,14 @@ def test_delete_todo():
 
 ### error version ###
 
+@pytest.mark.run_these_todoError_and_user
 # 存在しないidを指定された時に、404が帰ってくることを確認
 @temp_db
 def test_get_todo_by_id_error():
     response = client.get("/v1/todo/3")
     assert response.status_code == 404
 
+@pytest.mark.run_these_todoError_and_user
 # todo作成時に存在しないキーを指定した際にエラーになることを確認
 @temp_db
 def test_create_todo_error():
@@ -147,6 +163,7 @@ def test_create_todo_error():
     )
     assert response.status_code == 422
 
+@pytest.mark.run_these_todoError_and_user
 # 存在しないタグの選択
 @temp_db
 def test_error_update_todo():
@@ -158,6 +175,7 @@ def test_error_update_todo():
     response_check = client.get("/v1/todo/100")
     assert response_check.status_code == 404
 
+@pytest.mark.run_these_todoError_and_user
 # todo削除時に存在しないtodo idを指定した際にエラーになることを確認
 @temp_db
 def test_delete_todo_test():
